01ab1f814bc39cdef3736ff0c7fe0731b08ea454
zlolik <lolikandr@gmail.com>
2023-09-19 11:51:24 +0300

    fix package setup

--- setup.py
+++ setup.py
@@ -1,20 +1,16 @@
-import glob
 from distutils.core import setup
 
-# get all of the scripts
-scripts = glob.glob('bin/*')
-
 setup(
-  description='A pure python-based utility to extract text and images '
-              'from docx files.',
-  author='Ankush Shah',
-  author_email='ankush.shah.nitk@gmail.com',
-  url='https://github.com/ankushshah89/python-docx2txt',
-  download_url='https://github.com/ankushshah89/python-docx2txt/tarball/0.8',
-  keywords=['python', 'docx', 'text', 'images', 'extract'],
-  scripts=scripts,
   name='x2txt',
   packages=['x2txt'],
   version='0.1',
+  description='Utilities to extract text from docx and xlsx files.',
+  author='zlolik',
+  author_email='lolikandr@gmail.com',
+  url='https://github.com/zlolik/python-x2txt',
+  download_url='https://github.com/zlolik/python-x2txt/tarball/0.1',
+  keywords=['python', 'docx', 'xlsx', 'text', 'images', 'extract'],
+  install_requires=['openpyxl']
+  scripts=['docx2txt', 'xlsx2txt'],
   classifiers=[],
 )
